6552c3509bd80b328939f50dce0dddedd42ef05c
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-01-24 15:25:07 +0100

    fix debug line class

--- src/meshes/line.ts
+++ src/meshes/line.ts
@@ -1,7 +1,11 @@
-import { vec3, vec4 } from 'gl-matrix'
+import { mat4, vec3, vec4 } from 'gl-matrix'
 import { Drawable } from '../lib/hwoa-rang-gl2/dist'
 
+import VERTEX_SHADER_SRC from '../shaders/uberShader.vert'
+import FRAGMENT_SHADER_SRC from '../shaders/uberShader.frag'
+
 export default class Line extends Drawable {
+  cameraUBOIndex: GLuint
   startVec3: vec3
   endVec3: vec3
 
@@ -11,23 +15,29 @@ export default class Line extends Drawable {
     endVec3: vec3,
     color: vec4 = [1, 0, 0, 1],
   ) {
-    super(gl, {
+    super(gl, VERTEX_SHADER_SRC, FRAGMENT_SHADER_SRC, {
       USE_SOLID_COLOR: true,
+    })
+
+    this.setUniform('u_solidColor', {
+      type: gl.FLOAT_VEC4,
+      value: new Float32Array([1, 0, 0, 1]),
+    })
+    this.setUniform('u_worldMatrix', {
+      type: gl.FLOAT_MAT4,
+      value: mat4.create() as Float32Array,
     })
 
     this.startVec3 = startVec3
     this.endVec3 = endVec3
 
-    const aPositionLocation = gl.getAttribLocation(
-      this.drawProgram,
-      'aPosition',
-    )
+    const aPositionLocation = gl.getAttribLocation(this.program, 'aPosition')
 
     const solidColorUniformLocation = gl.getUniformLocation(
-      this.drawProgram,
+      this.program,
       'solidColor',
     )
-    gl.useProgram(this.drawProgram)
+    gl.useProgram(this.program)
     gl.uniform4f(
       solidColorUniformLocation,
       color[0],
@@ -46,11 +56,14 @@ export default class Line extends Drawable {
     gl.enableVertexAttribArray(aPositionLocation)
     gl.vertexAttribPointer(aPositionLocation, 3, gl.FLOAT, false, 0, 0)
     gl.bindVertexArray(null)
+
+    this.cameraUBOIndex = gl.getUniformBlockIndex(this.program, 'Camera')
   }
 
   render() {
     const gl = this.gl
-    gl.useProgram(this.drawProgram)
+    gl.uniformBlockBinding(this.program, this.cameraUBOIndex, 0)
+    gl.useProgram(this.program)
     gl.bindVertexArray(this.vao)
     gl.drawArrays(gl.LINES, 0, 2)
     gl.bindVertexArray(null)
