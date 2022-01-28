751ae1b0292695f0cc7027d321d011969c6c4c0d
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-01-28 14:01:18 +0100

    add vite plugin compression

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
+import compress from 'vite-plugin-compress'
 import glsl from 'vite-plugin-glsl'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [glsl()],
+  plugins: [compress(), glsl()],
   server: {
     host: true,
   },
