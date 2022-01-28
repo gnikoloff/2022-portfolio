25c7ecfd64c878895f711dec7df710a80958e3e1
Georgi Nikolov <nikoloffgeorgi@gmail.com>
2022-01-28 14:44:21 +0100

    add no webgl2 message

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
-import compress from 'vite-plugin-compress'
+// import compress from 'vite-plugin-compress'
 import glsl from 'vite-plugin-glsl'
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  plugins: [compress(), glsl()],
+  plugins: [glsl()],
   server: {
     host: true,
   },
